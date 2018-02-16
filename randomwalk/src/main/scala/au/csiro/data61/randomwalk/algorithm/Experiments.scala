adcb4eb57a6379da75e5e71f2b3ca9e638a0ef06
Hooman Peiro Sajjad <shps@kth.se>
2018-02-16 19:02:45 +0100

    Fixed a bug.

--- randomwalk/src/main/scala/au/csiro/data61/randomwalk/algorithm/Experiments.scala
+++ randomwalk/src/main/scala/au/csiro/data61/randomwalk/algorithm/Experiments.scala
@@ -6,6 +6,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 import scala.util.Random
+import scala.util.control.Breaks._
 
 /**
   * Created by Hooman on 2018-02-16.
@@ -20,83 +21,86 @@ case class Experiments(context: SparkContext, config: Params) extends Serializab
     val vertices: Array[Int] = g1.map(_._1).collect()
     val numSteps = Array.ofDim[Int](config.numRuns, vertices.length)
     for (i <- 0 until config.numRuns) {
-      for (j <- 0 until vertices.length) {
-        val target = vertices(j)
-        val g2 = removeVertex(g1, target)
-        val init = rwalk.initRandomWalk(g2)
-        val paths = rwalk.firstOrderWalk(init)
-        //            println(s"Before adding $target")
-        //            checkGraphMap()
-        rwalk.buildGraphMap(g1)
-        println(s"Added vertex $target")
-        val afs1 = GraphMap.getNeighbors(target).map { case (v, _) => v }
-        config.rrType match {
-          case RrType.m1 => {
-            val init = rwalk.initRandomWalk(g1)
-            //        checkGraphMap()
-            val ns = computeNumSteps(init)
-            numSteps(i) = Array.fill(vertices.length)(ns)
-            val p = rwalk.firstOrderWalk(init)
-            //          val summary = p.map(a => (a.head, (1, a.length))).reduceByKey((a,b)=>(a
-            // ._1+b._1,a._2+b._2))
-            //          println(s"Partial Paths: ${summary.count()}\n${summary.collect()
-            // .mkString("
-            // ")}")
-            fm.save(p, s"${config.rrType.toString}-$i")
-          }
-          case RrType.m2 => {
-            //            println(s"+$target -> ${afs1.mkString(" ")}")
-            //            checkGraphMap()
-            val fWalkers: Array[(Int, Array[Int])] = filterUniqueWalkers(
-              paths, afs1).collect() ++ Array((target, Array(target)))
-            //            println(s"Unique Walkers: ${fWalkers.length}\tdistinct: ${fWalkers
-            // .map(_._1).distinct.length}")
-            val walkers: RDD[(Int, Array[Int])] = context.parallelize(Array.fill(config
-              .numWalks)(fWalkers).flatMap(a => a), numSlices = config.rddPartitions)
-            //            println(s"Walkers: ${walkers.count()}\tdistinct: ${walkers.map(_._1)
-            // .distinct().count()}\nEach Key: ${walkers.map { case (a, _) => (a, 1) }
-            // .reduceByKey(_ + _).collect().mkString(" ")}")
-            val ns = computeNumSteps(walkers)
-            numSteps(i)(j) = ns
-            val pp = rwalk.firstOrderWalk(walkers)
-            //            val summary = pp.map(a => (a.head, (1, a.length))).reduceByKey((a,b)
-            // =>(a._1+b._1,a._2+b._2))
-            //            println(s"Partial Paths: ${summary.count()}\n${summary.collect()
-            // .mkString(" ")}")
-            val aws = fWalkers.map(tuple => tuple._1)
-            val up = paths.filter { case p =>
-              !aws.contains(p.head)
+      breakable {
+        for (j <- 0 until vertices.length) {
+          val target = vertices(j)
+          val g2 = removeVertex(g1, target)
+          val init = rwalk.initRandomWalk(g2)
+          val paths = rwalk.firstOrderWalk(init)
+          //            println(s"Before adding $target")
+          //            checkGraphMap()
+          rwalk.buildGraphMap(g1)
+          println(s"Added vertex $target")
+          val afs1 = GraphMap.getNeighbors(target).map { case (v, _) => v }
+          config.rrType match {
+            case RrType.m1 => {
+              val init = rwalk.initRandomWalk(g1)
+              //        checkGraphMap()
+              val ns = computeNumSteps(init)
+              numSteps(i) = Array.fill(vertices.length)(ns)
+              val p = rwalk.firstOrderWalk(init)
+              //          val summary = p.map(a => (a.head, (1, a.length))).reduceByKey((a,b)=>(a
+              // ._1+b._1,a._2+b._2))
+              //          println(s"Partial Paths: ${summary.count()}\n${summary.collect()
+              // .mkString("
+              // ")}")
+              fm.save(p, s"${config.rrType.toString}-$i")
+              break
             }
+            case RrType.m2 => {
+              //            println(s"+$target -> ${afs1.mkString(" ")}")
+              //            checkGraphMap()
+              val fWalkers: Array[(Int, Array[Int])] = filterUniqueWalkers(
+                paths, afs1).collect() ++ Array((target, Array(target)))
+              //            println(s"Unique Walkers: ${fWalkers.length}\tdistinct: ${fWalkers
+              // .map(_._1).distinct.length}")
+              val walkers: RDD[(Int, Array[Int])] = context.parallelize(Array.fill(config
+                .numWalks)(fWalkers).flatMap(a => a), numSlices = config.rddPartitions)
+              //            println(s"Walkers: ${walkers.count()}\tdistinct: ${walkers.map(_._1)
+              // .distinct().count()}\nEach Key: ${walkers.map { case (a, _) => (a, 1) }
+              // .reduceByKey(_ + _).collect().mkString(" ")}")
+              val ns = computeNumSteps(walkers)
+              numSteps(i)(j) = ns
+              val pp = rwalk.firstOrderWalk(walkers)
+              //            val summary = pp.map(a => (a.head, (1, a.length))).reduceByKey((a,b)
+              // =>(a._1+b._1,a._2+b._2))
+              //            println(s"Partial Paths: ${summary.count()}\n${summary.collect()
+              // .mkString(" ")}")
+              val aws = fWalkers.map(tuple => tuple._1)
+              val up = paths.filter { case p =>
+                !aws.contains(p.head)
+              }
 
-            //            println(s"Unaffected paths: ${up.count()}\t${up.map(a => (a.head, 1))
-            // .reduceByKey(_ + _).collect().mkString(" ")}")
-            val np = up.union(pp)
-            //            val summary2 = np.map(a => (a.head, (1, a.length))).reduceByKey((a,b)
-            // =>(a._1+b._1,a._2+b._2))
-            //            println(s"Partial Paths: ${summary2.count()}\n${summary2.collect()
-            // .mkString(" ")}")
-            fm.save(np, s"${config.rrType.toString}-v${target.toString}-$i")
+              //            println(s"Unaffected paths: ${up.count()}\t${up.map(a => (a.head, 1))
+              // .reduceByKey(_ + _).collect().mkString(" ")}")
+              val np = up.union(pp)
+              //            val summary2 = np.map(a => (a.head, (1, a.length))).reduceByKey((a,b)
+              // =>(a._1+b._1,a._2+b._2))
+              //            println(s"Partial Paths: ${summary2.count()}\n${summary2.collect()
+              // .mkString(" ")}")
+              fm.save(np, s"${config.rrType.toString}-v${target.toString}-$i")
+            }
+            case RrType.m3 => {
+              val walkers = filterSplitPaths(paths, afs1).union(rwalk.initWalker(target))
+              val ns = computeNumSteps(walkers)
+              numSteps(i)(j) = ns
+              val partialPaths = rwalk.firstOrderWalk(walkers)
+              val unaffectedPaths = filterUnaffectedPaths(paths, afs1)
+              val newPaths = unaffectedPaths.union(partialPaths)
+              fm.save(newPaths, s"${config.rrType.toString}-v${target.toString}-$i")
+            }
+            case RrType.m4 => {
+              val walkers = filterWalkers(paths, afs1).union(rwalk.initWalker(target))
+              val ns = computeNumSteps(walkers)
+              numSteps(i)(j) = ns
+              val partialPaths = rwalk.firstOrderWalk(walkers)
+              val unaffectedPaths = filterUnaffectedPaths(paths, afs1)
+              val newPaths = unaffectedPaths.union(partialPaths)
+              fm.save(newPaths, s"${config.rrType.toString}-v${target.toString}-$i")
+            }
           }
-          case RrType.m3 => {
-            val walkers = filterSplitPaths(paths, afs1).union(rwalk.initWalker(target))
-            val ns = computeNumSteps(walkers)
-            numSteps(i)(j) = ns
-            val partialPaths = rwalk.firstOrderWalk(walkers)
-            val unaffectedPaths = filterUnaffectedPaths(paths, afs1)
-            val newPaths = unaffectedPaths.union(partialPaths)
-            fm.save(newPaths, s"${config.rrType.toString}-v${target.toString}-$i")
-          }
-          case RrType.m4 => {
-            val walkers = filterWalkers(paths, afs1).union(rwalk.initWalker(target))
-            val ns = computeNumSteps(walkers)
-            numSteps(i)(j) = ns
-            val partialPaths = rwalk.firstOrderWalk(walkers)
-            val unaffectedPaths = filterUnaffectedPaths(paths, afs1)
-            val newPaths = unaffectedPaths.union(partialPaths)
-            fm.save(newPaths, s"${config.rrType.toString}-v${target.toString}-$i")
-          }
+
         }
-
       }
     }
 
