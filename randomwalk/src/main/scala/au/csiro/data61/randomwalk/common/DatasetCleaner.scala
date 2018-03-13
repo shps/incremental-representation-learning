55410518a59579fd3a9ef8ddee0feb1956583384
Hooman Peiro Sajjad <shps@kth.se>
2018-03-13 18:28:48 +0100

    Fixed some problems related to deduplication and removing self references.

--- randomwalk/src/main/scala/au/csiro/data61/randomwalk/common/DatasetCleaner.scala
+++ randomwalk/src/main/scala/au/csiro/data61/randomwalk/common/DatasetCleaner.scala
@@ -9,6 +9,9 @@ import scala.collection.parallel.ParSeq
 object DatasetCleaner {
 
   def checkRedundantEdges(edges: ParSeq[(Int, Int)]) = {
+    val selfEdges = edges.count { case (a, b) => a == b }
+    if (selfEdges > 0)
+      throw new Exception(s"Number of self edges: $selfEdges")
     val rEdges = edges.flatMap { case (a, b) => Seq((a, b), (b, a)) }.distinct
     if (edges.size * 2 != rEdges.size) {
       throw new Exception(s"There are reversed edges. Expected: ${edges.size * 2}, Actual: " +
@@ -36,22 +39,31 @@ object DatasetCleaner {
   def convertJsonFile(config: Params) {
     val fm = FileManager(config)
     val coAuthors = fm.readJsonFile()
-    fm.saveCoAuthors(coAuthors.toList)
-    val authors = coAuthors.flatMap { case (src, dst, y) => Seq(src, dst) }
+    val filtered = coAuthors.filter(_._3 != 0).map { case (src, dst, y) =>
+      if (src < dst)
+        (src, dst, y)
+      else
+        (dst, src, y)
+    }.groupBy { case (src, dst, _) => (src, dst)
+    }.toSeq.map { case ((src, dst), duplicates) =>
+      (src, dst, duplicates.seq.sortWith(_._3 < _._3).head._3)
+    }.filter { case (a, b, _) => !a.contentEquals(b) }
+
+    val authors = filtered.flatMap { case (src, dst, _) => Seq(src, dst) }
       .distinct.zipWithIndex
     fm.saveIds(authors)
     val idMaps = authors.toMap
-    // saves as indirected, removes duplicates.
-    val filtered = coAuthors.filter(_._3 != 0).map { case (a1, a2, y) =>
+    val coAuthorIded = filtered.map { case (a1, a2, y) =>
       val src = idMaps.getOrElse(a1, throw new Exception)
       val dst = idMaps.getOrElse(a2, throw new Exception)
       if (src < dst)
         (src, dst, y)
       else
         (dst, src, y)
-    }.seq.sortWith(_._3 < _._3).par.distinct
-    fm.saveCoAuthors(filtered)
-    val groups = filtered.groupBy(_._3)
+    }.seq.sortWith(_._3 < _._3).par
+    // saves as indirected, removes duplicates.
+    fm.saveCoAuthors(coAuthorIded)
+    val groups = coAuthorIded.groupBy(_._3)
     val sums = groups.map { case (y, edges) =>
       (y, edges.length)
     }.toSeq.seq.sortWith(_._1 < _._1).map { case (y, n) => s"$y\t$n" }.mkString("\n")
@@ -63,11 +75,12 @@ object DatasetCleaner {
     }.toSeq.seq.sortWith(_._1 < _._1).map { case (y, n) => s"$y\t$n" }.mkString("\n")
     fm.saveNumAuthors(ua, "unique-authors-per-year")
 
-//    val ra = groups.map { case (y, edges) =>
-//      val count = edges.flatMap { case (src, dst, _) => Seq((src, dst),(dst, src)) }.distinct.length
-//      (y, edges.length*2 - count)
-//    }.toSeq.seq.sortWith(_._1 < _._1).map { case (y, n) => s"$y\t$n" }.mkString("\n")
-//    println(ra)
+    //    val ra = groups.map { case (y, edges) =>
+    //      val count = edges.flatMap { case (src, dst, _) => Seq((src, dst),(dst, src)) }
+    // .distinct.length
+    //      (y, edges.length*2 - count)
+    //    }.toSeq.seq.sortWith(_._1 < _._1).map { case (y, n) => s"$y\t$n" }.mkString("\n")
+    //    println(ra)
   }
 
 }
