04c735e142b1faf79dc7c33a2386ac2cd537ad7c
Hooman Peiro Sajjad <shps@kth.se>
2018-04-13 16:21:16 +0200

    Fixed a bug in train.py.

--- randomwalk/src/main/python/train.py
+++ randomwalk/src/main/python/train.py
@@ -18,6 +18,7 @@ from sklearn.manifold import TSNE
 from src.main.python.minibatch import MiniBatch
 
 flags = tf.app.flags
+FLAGS = flags.FLAGS
 
 flags.DEFINE_float('learning_rate', 0.1, 'initial learning rate.')
 flags.DEFINE_string('train_prefix', '',
@@ -32,7 +33,7 @@ flags.DEFINE_integer('avg_loss_comp_freq', 1000, 'Frequency of average loss comp
 
 flags.DEFINE_string('base_log_dir', '.', 'base directory for logging and saving embeddings')
 flags.DEFINE_string('input_dir', '.', 'Input data directory.')
-flags.DEFINE_string('train_file', '', 'Input train file name.')
+flags.DEFINE_string('train_file', 'blah.txt', 'Input train file name.')
 flags.DEFINE_string('existing_vocabs_file', '', 'Input existing vocab file name.')
 flags.DEFINE_string('delimiter', '\t', 'Delimiter.')
 flags.DEFINE_integer('print_every', 50, "How often to print training info.")
@@ -42,15 +43,15 @@ flags.DEFINE_integer('seed', 1234, "Seed for random generator.")
 
 def read_data(fname):
     rws = np.concatenate([
-        np.loadtxt(f.open(), delimiter=flags.delimiter, dtype=int)
-        for f in Path(flags.input_dir).glob(fname)
+        np.loadtxt(f.open(), delimiter=FLAGS.delimiter, dtype=int)
+        for f in Path(FLAGS.input_dir).glob(fname)
         if f.stat().st_size > 0])
     return rws
 
 
 def load_data(file_name):
-    np.random.seed(seed=flags.seed)
-    data = read_data(file_name) - flags.start_id
+    np.random.seed(seed=FLAGS.seed)
+    data = read_data(file_name) - FLAGS.start_id
     print('Data size', len(data))
     return data
 
@@ -58,7 +59,7 @@ def load_data(file_name):
 def read_vocabs(fname):
     rws = np.concatenate([
         np.loadtxt(f.open(), dtype=int)  # accepts only the first label
-        for f in Path(flags.input_dir).glob(fname)
+        for f in Path(FLAGS.input_dir).glob(fname)
         if f.stat().st_size > 0
     ])
 
@@ -71,8 +72,8 @@ def choose_valid_examples(existing_vocabs):
     # validation samples to the words that have a low numeric ID, which by
     # construction are also the most frequent. These 3 variables are used only for
     # displaying model accuracy, they don't affect calculation.
-    np.random.seed(flags.seed)
-    valid_examples = np.random.choice(existing_vocabs, flags.valid_size, replace=False)
+    np.random.seed(FLAGS.seed)
+    valid_examples = np.random.choice(existing_vocabs, FLAGS.valid_size, replace=False)
     return valid_examples
 
 
@@ -82,8 +83,8 @@ def train(minibatch, valid_examples):
     with graph.as_default():
         # Input data.
         with tf.name_scope('inputs'):
-            train_inputs = tf.placeholder(tf.int32, shape=[flags.batch_size])
-            train_contexts = tf.placeholder(tf.int32, shape=[flags.batch_size, 1])
+            train_inputs = tf.placeholder(tf.int32, shape=[FLAGS.batch_size])
+            train_contexts = tf.placeholder(tf.int32, shape=[FLAGS.batch_size, 1])
             valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
         # Ops and variables pinned to the CPU because of missing GPU implementation
@@ -91,17 +92,17 @@ def train(minibatch, valid_examples):
             # Look up embeddings for inputs.
             with tf.name_scope('embeddings'):
                 embeddings = tf.Variable(
-                    tf.random_uniform([flags.vocab_size, flags.dim], -1.0, 1.0))
+                    tf.random_uniform([FLAGS.vocab_size, FLAGS.dim], -1.0, 1.0))
                 embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
             # Construct the variables for the NCE loss
             with tf.name_scope('weights'):
                 nce_weights = tf.Variable(
                     tf.truncated_normal(
-                        [flags.vocab_size, flags.dim],
-                        stddev=1.0 / math.sqrt(flags.dim)))
+                        [FLAGS.vocab_size, FLAGS.dim],
+                        stddev=1.0 / math.sqrt(FLAGS.dim)))
             with tf.name_scope('biases'):
-                nce_biases = tf.Variable(tf.zeros([flags.vocab_size]))
+                nce_biases = tf.Variable(tf.zeros([FLAGS.vocab_size]))
                 # Compute the average NCE loss for the batch.
                 # tf.nce_loss automatically draws a new sample of the negative labels each
                 # time we evaluate the loss.
@@ -114,13 +115,13 @@ def train(minibatch, valid_examples):
                         biases=nce_biases,
                         labels=train_contexts,
                         inputs=embed,
-                        num_sampled=flags.neg_sample_size,
-                        num_classes=flags.vocab_size))
+                        num_sampled=FLAGS.neg_sample_size,
+                        num_classes=FLAGS.vocab_size))
             # Add the loss value as a scalar to summary.
             tf.summary.scalar('loss', loss)
             # Construct the SGD optimizer using a learning rate of 1.0.
             with tf.name_scope('optimizer'):
-                optimizer = tf.train.GradientDescentOptimizer(flags.learning_rate).minimize(loss)
+                optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(loss)
             # Compute the cosine similarity between minibatch examples and all embeddings.
             norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keepdims=True))
             normalized_embeddings = embeddings / norm
@@ -142,7 +143,7 @@ def train(minibatch, valid_examples):
 
     with tf.Session(graph=graph) as session:
         # Open a writer to write summaries.
-        writer = tf.summary.FileWriter(flags.log_dir, session.graph)
+        writer = tf.summary.FileWriter(FLAGS.log_dir, session.graph)
 
         # We must initialize all variables before we use them.
         init.run()
@@ -173,7 +174,7 @@ def train(minibatch, valid_examples):
             if step == (minibatch.num_batches() - 1):
                 writer.add_run_metadata(run_metadata, 'step%d' % step)
 
-                if step % flags.avg_loss_comp_freq == 0:
+                if step % FLAGS.avg_loss_comp_freq == 0:
                     if step > 0:
                         average_loss /= 50
                     # The average loss is an estimate of the loss over the last 2000 batches.
@@ -181,7 +182,7 @@ def train(minibatch, valid_examples):
                     average_loss = 0
 
                 # Note that this is expensive (~20% slowdown if computed every 500 steps)
-                if step % flags.sim_comp_freq == 0:
+                if step % FLAGS.sim_comp_freq == 0:
                     sim = similarity.eval()
                     for i in range(len(valid_examples)):
                         valid_word = valid_examples[i]
@@ -196,18 +197,18 @@ def train(minibatch, valid_examples):
         final_embeddings = normalized_embeddings.eval()
         # Write corresponding labels for the embeddings.
 
-        with open(flags.log_dir + '/metadata.tsv', 'w') as f:
-            for i in range(flags.vocab_size):
-                f.write(str(i + flags.start_id) + '\n')
+        with open(FLAGS.log_dir + '/metadata.tsv', 'w') as f:
+            for i in range(FLAGS.vocab_size):
+                f.write(str(i + FLAGS.start_id) + '\n')
 
         # Save the model for checkpoints.
-        saver.save(session, os.path.join(flags.log_dir, 'model.ckpt'))
+        saver.save(session, os.path.join(FLAGS.log_dir, 'model.ckpt'))
 
         # Create a configuration for visualizing embeddings with the labels in TensorBoard.
         config = projector.ProjectorConfig()
         embedding_conf = config.embeddings.add()
         embedding_conf.tensor_name = embeddings.name
-        embedding_conf.metadata_path = os.path.join(flags.log_dir, 'metadata.tsv')
+        embedding_conf.metadata_path = os.path.join(FLAGS.log_dir, 'metadata.tsv')
         projector.visualize_embeddings(writer, config)
 
         writer.close()
@@ -216,10 +217,10 @@ def train(minibatch, valid_examples):
 
 def main(argv=None):
     print("Loading the input data..")
-    train_data = load_data(flags.train_file)
+    train_data = load_data(FLAGS.train_file)
     print("Finished loading.")
-    minibatch = MiniBatch(train_data, flags.batch_size, flags.seed)
-    existing_vocabs = read_vocabs(flags.existing_vocabs_file)
+    minibatch = MiniBatch(train_data, FLAGS.batch_size, FLAGS.seed)
+    existing_vocabs = read_vocabs(FLAGS.existing_vocabs_file)
     valid_examples = choose_valid_examples(existing_vocabs)
     final_embeddings = train(minibatch, valid_examples)
 
